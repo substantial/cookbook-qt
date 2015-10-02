2c18ef21f2bd55a870e757e6669e0b740b73586a
Robin Clowers <robin.clowers@gmail.com>
2015-10-01 17:03:40 -0700

    Update cookbook metadata

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,7 @@ maintainer_email "shaun@substantial.com"
 license          "All rights reserved"
 description      "Installs/Configures qt"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
+
+recipe 'qt', 'Installs qt4'
+recipe 'qt::qt5', 'Installs qt5'
