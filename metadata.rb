7367c199987098ae209a472e74ed6fb024084306
Robin Clowers <robin.clowers@gmail.com>
2015-10-05 14:34:03 -0700

    Add the sdk team repo for Ubuntu 12.04

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,9 @@ maintainer_email "shaun@substantial.com"
 license          "All rights reserved"
 description      "Installs/Configures qt"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
+
+depends 'apt'
 
 recipe 'qt', 'Installs qt4'
 recipe 'qt::qt5', 'Installs qt5'
