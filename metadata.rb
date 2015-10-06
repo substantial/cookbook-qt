ecf4f815e15fbc8c9e5d33747a196ae8b9f9e84f
Robin Clowers <robin.clowers@gmail.com>
2015-10-06 09:41:29 -0700

    Bumb minor version number

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "shaun@substantial.com"
 license          "All rights reserved"
 description      "Installs/Configures qt"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.1.0"
 
 depends 'apt'
 
